aad7181abb4e4c7ee711ad67d5d645126384788d
Yulia Milich <milichy@post.bgu.ac.il>
2026-04-24 10:58:03 +0000

    task3: simple and expected version

--- kernel/proc.c
+++ kernel/proc.c
@@ -746,7 +746,6 @@ int co_yield(int pid, int value)
   struct proc *p;
   struct proc *my_p = myproc();
   struct cpu *c = mycpu();
-  int returnval;
 
   if (pid <= 0 || pid == my_p->pid)
   {
@@ -769,9 +768,7 @@ int co_yield(int pid, int value)
 
   if (p->state == SLEEPING && p->chan == my_p) // second to reach co_yield.
   {
-    my_p->trapframe->a0 = value;
-
-    returnval = p->trapframe->a0;
+    p->trapframe->a0 = value;
     // switch
     my_p->chan = p;
     my_p->state = SLEEPING;
@@ -784,42 +781,19 @@ int co_yield(int pid, int value)
 
     // before: hold only p->lock. after: holds only my_p->lock.
     swtch(&my_p->context, &p->context);
+    release(&my_p->lock);
   }
   else // first to reach co_yield.
   {
-    my_p->trapframe->a0 = value;
-    if (p->state == RUNNABLE)
-    {
-      // do swtch
-      my_p->chan = p;
-      my_p->state = SLEEPING;
+    release(&my_p->lock);
+    sleep(p, &p->lock);
 
-      p->chan = 0;
-      p->state = RUNNING;
-      c->proc = p;
-
-      release(&my_p->lock);
-      swtch(&my_p->context, &p->context);
-      acquire(&p->lock);
-    }
-    else
-    {
-      release(&my_p->lock);
-      sleep(p, &p->lock);
-      acquire(&my_p->lock);
-    }
-
-    returnval = p->trapframe->a0;
+    p->trapframe->a0 = value;
 
     p->chan = 0;
     p->state = RUNNABLE;
     release(&p->lock);
   }
 
-  if (!holding(&my_p->lock))
-    panic("co_yield my_p->lock");
-
-  release(&my_p->lock);
-
-  return returnval;
+  return my_p->trapframe->a0;
 }
